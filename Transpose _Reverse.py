af51d34381b34a39be55942b62429ba6fd3c2b58
Hamsini Sripaada <hamsinisripada@gmail.com>
2026-06-08 20:45:07 +0530

    Add example for matrix transposition and reversal
    Added example input and output for transposing and reversing a matrix.

--- Transpose _Reverse.py
+++ Transpose _Reverse.py
@@ -10,3 +10,27 @@ class Solution:
         # Reverse each row
         for row in matrix:
             row.reverse()
+
+
+"""Input:
+
+matrix = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+]
+
+After transpose:
+
+[
+    [1,4,7],
+    [2,5,8],
+    [3,6,9]
+]
+
+After reversing rows:
+
+[
+    [7,4,1],
+    [8,5,2],
+    [9,6,3]  """

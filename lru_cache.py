b4a6a95c64c42bea7991f5ee0ba5571f0ffb878a
Hamsini Sripaada <hamsinisripada@gmail.com>
2026-06-20 20:57:30 +0530

    add O(1) LRU Cache implementation for efficient cache eviction
    Implement LRU Cache with get and put methods.
    Implement O(1) LRU Cache using Hash Map and Doubly Linked List

---
+++ lru_cache.py
@@ -0,0 +1,56 @@
+class Node:
+    def __init__(self, key=0, value=0):
+        self.key = key
+        self.value = value
+        self.prev = None
+        self.next = None
+
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = {}
+
+        self.left = Node()   # LRU
+        self.right = Node()  # MRU
+
+        self.left.next = self.right
+        self.right.prev = self.left
+
+    def remove(self, node):
+        prev_node = node.prev
+        next_node = node.next
+
+        prev_node.next = next_node
+        next_node.prev = prev_node
+
+    def insert(self, node):
+        prev_node = self.right.prev
+        next_node = self.right
+
+        prev_node.next = node
+        next_node.prev = node
+
+        node.prev = prev_node
+        node.next = next_node
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            self.remove(self.cache[key])
+            self.insert(self.cache[key])
+            return self.cache[key].value
+
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.remove(self.cache[key])
+
+        self.cache[key] = Node(key, value)
+        self.insert(self.cache[key])
+
+        if len(self.cache) > self.capacity:
+            lru = self.left.next
+            self.remove(lru)
+            del self.cache[lru.key]
